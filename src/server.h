736322ca7935d77d015ada714df07aa927be2195
Anay-Mahajan <anaymahajan2005@gmail.com>
2025-11-01 00:24:28 +0530

    Intial Setup

---
+++ src/server.h
@@ -0,0 +1,9 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+namespace server {
+    // Starts a simple TCP server on the given port
+    void start(int port);
+}
+
+#endif
---
+++ src/utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+// Simple logging helper
+void logMessage(const std::string &msg);
+
+#endif
