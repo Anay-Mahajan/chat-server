dc92ffd61e17d9244c4748b0a6c71941bfd824cb
Anay-Mahajan <anaymahajan2005@gmail.com>
2025-11-26 00:08:17 +0530

    Minor fix and performed stress test

--- src/server.cpp
+++ src/server.cpp
@@ -5,105 +5,241 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <string>
-#include<cstring>
+#include <cstring>
 #include <iostream>
 #include <string>
-#include<thread>
-#include<mutex>
- namespace server{
-    void start(int port){
-        int sd=socket(PF_INET,SOCK_STREAM,0);
-        sd_=sd;
-        struct addrinfo hints{};
-        struct addrinfo *resutlt;
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family=PF_INET;
-        hints.ai_socktype=SOCK_STREAM;
-        hints.ai_flags=AI_PASSIVE;
-        int status =getaddrinfo(NULL,std::to_string(port).c_str(),&hints,&resutlt);
-        if(status==-1){
-         std::cout<<"Handle Address Info\n";
+#include <thread>
+#include <mutex>
+#include <csignal>
+#include <vector>
+namespace server
+{
+  void start(int port)
+  {
+    signal(SIGPIPE, SIG_IGN);
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd < 0)
+    {
+      perror("socket");
+      return;
+    }
+    sd_ = sd;
+    int opt = 1;
+    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+      perror("setsockopt");
+      close(sd);
+      return;
+    }
+    struct addrinfo hints;
+    struct addrinfo *result;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    int status = getaddrinfo(nullptr, std::to_string(port).c_str(), &hints, &result);
+    if (status != 0)
+    {
+      std::cerr << "getaddrinfo: " << gai_strerror(status) << "\n";
+      close(sd);
+      return;
+    }
+    if (bind(sd, result->ai_addr, result->ai_addrlen) < 0)
+    {
+      perror("bind");
+      freeaddrinfo(result);
+      close(sd);
+      return;
+    }
+    freeaddrinfo(result);
+    if (listen(sd, SOMAXCONN) < 0)
+    {
+      perror("listen");
+      close(sd);
+      return;
+    }
+    std::cout << "Listening on port " << port << std::endl;
+    accept_new_connection(sd_);
+    std::cout<<"Total unsuccessfull message "<<count<<'\n';
+    close(sd);
+  }
+
+  void accept_new_connection(int sd)
+  {
+    while(true){
+      struct sockaddr_in client_addr;
+      socklen_t client_len = sizeof(client_addr);
+      int client_sd = accept(sd, (struct sockaddr *)&client_addr, &client_len);
+      if (client_sd == -1)
+      {
+        perror("Accept failed");
+        continue;
+      }
+      logMessage("New connection Accepted");
+      std::thread(handle_client, client_sd).detach();
+    }
+  }
+  void close_connection(int sd)
+  {
+    std::unique_lock<std::shared_mutex> lock(mp_mutex);
+    auto it = sd_to_username.find(sd);
+    if (it != sd_to_username.end())
+    {
+      username_to_sd.erase(it->second);
+      sd_to_username.erase(it);
+    }
+  }
+  bool safe_send(int sd, std::string data){
+    ssize_t n = send(sd, data.c_str(), data.size(), MSG_NOSIGNAL);
+    return n > 0;
+  }
+
+  void route_message(int from_sd, int to_sd, std::string message)
+  {
+      std::string from_user;
+      {
+      std::shared_lock<std::shared_mutex> lock(mp_mutex);
+      auto it = sd_to_username.find(from_sd);
+      if (it == sd_to_username.end())
+        return;
+      from_user = it->second;
+    }
+    std::string data = from_user + "->" + message + "\n";
+    if (!safe_send(to_sd, data)){
+      count++;
+      logMessage("Couldn't Send Messsage"+std::to_string(count));
+      std::set<int>::iterator it;
+      {
+        std::shared_lock<std::shared_mutex>lock(closed_mutex);
+        it=closed_connection.find(to_sd);
+      }
+      if(it==closed_connection.end()){
+        std::unique_lock<std::shared_mutex> lock(closed_mutex);
+        closed_connection.insert(to_sd);
+      }
+    }
+  }
+
+  void broadcast(int from_sd, std::string message)
+  {
+    std::string from_user;
+    std::vector<int> targets;{
+      std::shared_lock<std::shared_mutex> lock(mp_mutex);
+      auto it = sd_to_username.find(from_sd);
+      if (it == sd_to_username.end())
+        return;
+
+      from_user = it->second;
+
+      for (const auto &[user, sd] : username_to_sd)
+      {
+        if (sd != from_sd)
+          targets.push_back(sd);
+      }
+    }
+    std::string data =from_user + "->" + message + "\n";
+    for (int sd : targets)
+    {
+      if (!safe_send(sd, data))
+      {
+        count++;
+        logMessage("Couldn't Send Messsage"+std::to_string(count));
+        std::set<int>::iterator it;
+      {
+        std::shared_lock<std::shared_mutex>lock(closed_mutex);
+        it=closed_connection.find(sd);
+      }
+      if(it==closed_connection.end()){
+        std::unique_lock<std::shared_mutex> lock(closed_mutex);
+        closed_connection.insert(sd);
+      }
+      }
+    }
+  }
+
+  void handle_route(int sd, std::string message)
+  {
+    int i = 1;
+    ;
+    std::string route = "";
+    while (i < message.size() && message[i] != ' ')
+    {
+      route += message[i];
+      i++;
+    }
+    if (i == message.size())
+    {
+      logMessage("Invalid Message\n");
+      return;
+    }
+    i++;
+    std::string raw_message = "";
+    while (i < message.size())
+    {
+      raw_message += message[i];
+      i++;
+    }
+
+    if (route == "advertize")
+    {
+      {
+        std::unique_lock<std::shared_mutex> lock(mp_mutex);
+        username_to_sd[raw_message] = sd;
+        sd_to_username[sd] = raw_message;
+      }
+      logMessage(std::to_string(sd) + " " + raw_message);
+    }
+    else if (route == "all")
+    {
+      logMessage(std::to_string(sd) + " " + raw_message);
+      broadcast(sd, raw_message);
+    }
+    else
+    {
+      int target_sd;
+
+      {
+        std::shared_lock<std::shared_mutex> lock(mp_mutex);
+        auto it = username_to_sd.find(route);
+        if (it == username_to_sd.end())
+        {
+          logMessage("Could Not find " + route);
+          return;
         }
-       status=bind(sd,resutlt->ai_addr,resutlt->ai_addrlen);
-       if(status==-1){
-         std::cout<<"Handle Bind\n";
-       }
-         status =listen(sd,10);
-         if(status==-1){
-            std::cout<<"Handle Listen\n";
-         }
-         else{
-            std::cout<<"Listening on Port 8080\n";
-         }
-         accept_new_connection(sd);
-         close(sd);
+        target_sd = it->second;
+      }
+      logMessage(std::to_string(sd) + " " +std::to_string(target_sd) + " " +raw_message);
+      route_message(sd, target_sd, raw_message);
     }
-    void accept_new_connection(int sd){
-      for(;;){
-          struct sockaddr_in client_addr;
-          socklen_t client_len = sizeof(client_addr);
-          int client_sd = accept(sd, (struct sockaddr*)&client_addr, &client_len);
-         if (client_sd == -1) {
-            perror("Accept failed");
-            continue;
-         }
-         logMessage("New connection Accepted");  
-         std::thread(handle_client,client_sd).detach();
+  }
+  void handle_client(int client_sd)
+  {
+    char buffer[4096];
+    std::string incoming;
+    while (true)
+    {
+      memset(buffer, 0, sizeof(buffer));
+      int bytes_received;
+      bytes_received = recv(client_sd, buffer, sizeof(buffer), 0);
+      if (bytes_received <= 0)
+      {
+        std::unique_lock<std::shared_mutex>lock(closed_mutex);
+        closed_connection.insert(client_sd);
+        break;
+      }
+      incoming.append(buffer, bytes_received);
+      size_t pos;
+      while ((pos = incoming.find('\n')) != std::string::npos)
+      {
+        std::string message = incoming.substr(0, pos);
+        incoming.erase(0, pos + 1);
+        if (!message.empty())
+        {
+          handle_route(client_sd, message);
+        }
       }
     }
-    void route_message(int sd_1,int sd_2,std::string message){
-      std::string data = "@" + sd_to_username[sd_1] + " " + message;
-      send(sd_2, data.c_str(), data.length(), 0);
-    }
-    void broadcast(int sd_from,std::string message){
-      std::string data = "@" +sd_to_username[sd_from]+" "+message;
-      for(auto&[user,sd]:username_to_sd){
-         if(sd!=sd_from)   send(sd,data.c_str(),data.length(),0);
-      }
-    }
-    void handle_route(int sd,std::string message){
-       int i=1;;
-       std::string route="";
-       while(message[i]!=' '){
-         route+=message[i];
-          i++;
-       }
-       i++;
-       std::string raw_message="";
-       while (i<message.size()){
-         raw_message+=message[i];
-         i++;
-       }
-       
-       if(route=="advertize"){
-         username_to_sd[raw_message]=sd;
-         sd_to_username[sd]=raw_message;
-         logMessage(std::to_string(sd)+" "+raw_message);
-       }
-       else if(route=="all"){
-         logMessage(std::to_string(sd)+" "+raw_message);
-         broadcast(sd,raw_message);
-       }
-       else{
-         if(username_to_sd.find(route)!=username_to_sd.end()){
-            logMessage(std::to_string(sd)+" "+std::to_string(username_to_sd[route])+" "+raw_message);
-            route_message(sd,username_to_sd[route],raw_message);
-         }
-         else{
-            logMessage("Could Not find "+route);
-         }
-       }
-    }
-    void handle_client(int client_sd){
-       char buffer[4096];
-       while (true){
-          memset(buffer, 0, sizeof(buffer));
-          int bytes_received;
-          bytes_received = recv(client_sd, buffer, sizeof(buffer), 0);
-          if (bytes_received > 0){
-            std::string message= std::string(buffer,bytes_received);
-            std::thread(handle_route,client_sd,message).detach();    
-          }
-       }
-    }
- }
+    close_connection(client_sd);
+    close(client_sd);
+  }
+}
---
+++ stress_test.py
@@ -0,0 +1,102 @@
+import socket
+import threading
+import time
+import random
+
+SERVER_IP = "192.168.1.2"
+SERVER_PORT = 8080
+
+NUM_CLIENTS = 1000
+MESSAGES_PER_CLIENT = 10
+
+start_event = threading.Event()
+
+successful_sends = 0
+failed_sends = 0
+lock = threading.Lock()
+
+
+def client_task(client_id):
+    global successful_sends, failed_sends
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SERVER_IP, SERVER_PORT))
+        s.settimeout(5)
+
+        username = f"user{client_id}"
+        s.sendall(f"@advertize {username}\n".encode())
+
+        # Start background receiver to avoid send buffer blockage
+        def recv_loop():
+            try:
+                while True:
+                    data = s.recv(4096)
+                    if not data:
+                        break
+            except:
+                pass
+
+        threading.Thread(target=recv_loop, daemon=True).start()
+
+        # Wait until all clients are ready
+        start_event.wait()
+
+        for i in range(MESSAGES_PER_CLIENT):
+            msg = f"@all message {i} from {username}\n"
+            try:
+                s.sendall(msg.encode())
+                with lock:
+                    successful_sends += NUM_CLIENTS - 1
+            except Exception:
+                with lock:
+                    failed_sends += NUM_CLIENTS - 1
+                break
+
+            time.sleep(random.uniform(0.001, 0.005))
+
+        time.sleep(1)
+        s.close()
+
+    except Exception:
+        with lock:
+            failed_sends += NUM_CLIENTS - 1
+
+
+def main():
+    threads = []
+
+    print(f"Starting {NUM_CLIENTS} clients...")
+
+    start_time = time.time()
+
+    for i in range(NUM_CLIENTS):
+        t = threading.Thread(target=client_task, args=(i,))
+        threads.append(t)
+        t.start()
+
+    # Give all clients time to connect
+    time.sleep(2)
+
+    print("All clients connected. Starting message send phase.")
+    start_event.set()
+
+    for t in threads:
+        t.join()
+
+    end_time = time.time()
+
+    total_messages = NUM_CLIENTS * MESSAGES_PER_CLIENT * (NUM_CLIENTS - 1)
+
+    print("\n===== Stress Test Results =====")
+    print(f"Total fan-out messages attempted: {total_messages}")
+    print(f"Successful sends: {successful_sends}")
+    print(f"Failed sends: {failed_sends}")
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+
+    if end_time > start_time:
+        print(f"Throughput: {total_messages / (end_time - start_time):,.0f} msgs/sec")
+
+
+if __name__ == "__main__":
+    main()
